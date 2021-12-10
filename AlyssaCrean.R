2e0a29d19c16c6f28ca7c50356a8dca5dfb2e9a7
alyssac59 <95929177+alyssac59@users.noreply.github.com>
2021-12-10 11:43:07 -0500

    Create AlyssaCrean.R

---
+++ AlyssaCrean.R
@@ -0,0 +1,9 @@
+Xbar <- numeric(10000)
+#set.seed(300)
+for (i in 1:10000)
+{
+    x <- rexp(1000, rate = 1/15)
+    Xbar[i] <- mean(x)
+}
+hist(Xbar, col= "blue", ylim=c(0,1),main="Simulated sampling distribution", xlab="means", freq=FALSE)
+lines(10:20, dnorm(10:20, mean(Xbar), sd(Xbar) ), col="orange",type = "b") #overlay pmf for Normal(17)
